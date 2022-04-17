e73b5d90637f2a22d1743e8c5850ce8c45ac00dd
Fatimah Moallim <f.m369@hotmail.com>
2022-04-17 04:21:50 +0300

    Fetch todo lists from database

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\TodoList;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_fetch_todo_list()
+    {
+        // Preparation / prepare
+        TodoList::create(['name' => 'my list']);
+
+        // Action / perform
+        $response = $this->getJson(route('todo-list.store'));
+        // dd($response->json());
+
+        // Assertion / predict
+        $this->assertEquals(1, count($response->json()));
+    }
+}
